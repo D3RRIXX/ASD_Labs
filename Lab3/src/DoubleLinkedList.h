80cfbbd6ada02c315353d06188e39fe54486a1d2
D3RRIXX <jenyazagevskii@gmail.com>
2024-11-25 12:30:10 +0200

    Started implementing double linked list

---
+++ Lab3/src/DoubleLinkedList.h
@@ -0,0 +1,37 @@
+﻿//
+// Created by Jenya on 20.11.2024.
+//
+
+#ifndef DOUBLELINKEDLIST_H
+#define DOUBLELINKEDLIST_H
+
+template<typename T>
+class DoubleLinkedList
+{
+public:
+	struct Node
+	{
+		T Value;
+		Node* Next;
+		Node* Previous;
+
+		explicit Node(const T& value) : Value(value) {}
+	};
+
+private:
+	Node* _head;
+	Node* _tail;
+
+public:
+	DoubleLinkedList() : _head(nullptr), _tail(nullptr) {}
+
+	Node* InsertAfter(const T& value, Node* node = nullptr);
+};
+
+template<typename T>
+typename DoubleLinkedList<T>::Node* DoubleLinkedList<T>::InsertAfter(const T& value, Node* node)
+{
+	
+}
+
+#endif //DOUBLELINKEDLIST_H
